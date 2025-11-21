235309879c159cf434c60180aaefcc5c141c8167
xiaoxiao194 <79774352+xiaoxiao194@users.noreply.github.com>
2025-11-21 10:54:52 +0800

    Update date_utils.py
    add wrong version

--- date_utils.py
+++ date_utils.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 def days_between(date1, date2):
-    # 故意改成错误的格式（一个用斜杠，一个用横杆）
+    # 故意写错格式
     d1 = datetime.strptime(date1, "%Y/%m/%d")
     d2 = datetime.strptime(date2, "%Y-%m-%d")
 
-    # 故意去掉 abs，导致负数结果
+    # 故意去掉 abs
     return (d2 - d1).days
